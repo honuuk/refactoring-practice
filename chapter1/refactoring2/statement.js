789bbc918174b3767f91c070dc87c72a80a565ac
jewook <jewook3617@gmail.com>
2021-07-20 11:18:49 +0900

    statement를 html로 출력하는 함수 추가

--- chapter1/refactoring2/statement.js
+++ chapter1/refactoring2/statement.js
@@ -14,32 +14,44 @@
 import * as fs from "fs";
 import createStatementData from "./createStatementData.js";
 
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
 function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays));
+  return renderHtml(createStatementData(invoice, plays));
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-  function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-    for (let perf of data.performances) {
-      // 청구내역 출력
-      result += `  ${perf.play.name}: ${usd(perf.amount)} (${
-        perf.audience
-      }석)\n`;
-    }
-
+  for (let perf of data.performances) {
     // 청구내역 출력
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-    return result;
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
+
+  // 청구내역 출력
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${usd(data.totalVolumeCredits)}</em>점</p>\n`;
+  return result;
 }
 
 const invoice = JSON.parse(
