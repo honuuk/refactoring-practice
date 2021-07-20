0bc2c39f46fc735f297fda21b92e103c639b3598
jewook <jewook3617@gmail.com>
2021-07-20 09:54:06 +0900

    amountFor 함수도 statement 하위로 이동 후 매개변수 데이터에 amount 정보 추가

--- chapter1/refactoring2.js
+++ chapter1/refactoring2.js
@@ -18,11 +18,37 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return result;
   }
 
   function renderPlainText(data, plays) {
@@ -74,32 +100,6 @@ function statement(invoice, plays) {
         currency: "USD",
         minimumFractionDigits: 2,
       }).format(aNumber / 100);
-    }
-
-    function amountFor(aPerformance) {
-      let result = 0;
-
-      switch (aPerformance.play.type) {
-        case "tragedy": // 비극
-          result = 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-
-        case "comedy": // 희극
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-
-        default:
-          throw new Error(`알 수 없는 장르: ${play.type}`);
-      }
-
-      return result;
     }
   }
 }
