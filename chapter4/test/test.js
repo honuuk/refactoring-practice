e51af27e3b5902eba6aff2d187cf2377f56f4c27
jewook <jewook3617@gmail.com>
2021-08-15 19:11:06 +0900

    공통 fixture 추출

--- chapter4/test/test.js
+++ chapter4/test/test.js
@@ -4,13 +4,16 @@ import Province from "../src/Province.js";
 import { sampleProvinceData } from "../src/index.js";
 
 describe("province", () => {
+  let asia;
+
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
   it("shortfall", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
 
   it("profit", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
