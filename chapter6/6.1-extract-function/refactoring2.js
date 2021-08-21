17422d69cfc1911e2dc8da406d11a08e678319a6
jewook <jewook3617@gmail.com>
2021-08-21 11:51:12 +0900

    recordDueDate() 함수 추출

--- chapter6/6.1-extract-function/refactoring2.js
+++ chapter6/6.1-extract-function/refactoring2.js
@@ -8,21 +8,23 @@ function printOwing(invoice) {
     outstanding += o.amout;
   }
 
-  // 마감일(dueDate)을 기록한다.
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
+
+function printBanner() {
+  console.log("*****************");
+  console.log("***** 고객 채무 *****");
+  console.log("*****************");
+}
+
+function recordDueDate(invoice) {
   const today = Clock.today;
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
-
-  printDetails(invoice, outstanding);
-
-  function printBanner() {
-    console.log("*****************");
-    console.log("***** 고객 채무 *****");
-    console.log("*****************");
-  }
 }
 
 function printDetails(invoice, outstanding) {
