16130181bf934d9887b9f0ab17036f60f119fdad
jewook <jewook3617@gmail.com>
2021-08-22 21:22:33 +0900

    get basePrice() 메서드로 추출

---
+++ chapter6/#3-extract-variable/example2/refactoring.js
@@ -0,0 +1,23 @@
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.basePrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 -
+      Math.min(this.basePrice * 0.1, 100)
+    );
+  }
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+}
