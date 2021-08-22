923d345c5f0cbe3a35856cbaa50d285c27831cfc
jewook <jewook3617@gmail.com>
2021-08-22 22:13:11 +0900

    6-5 example2 마이그레이션 절차에 따라 매개변수 추가하기

---
+++ chapter6/#5-change-function-declaration/example2/refactoring.js
@@ -0,0 +1,16 @@
+const assert = require("assert");
+
+class Book {
+  constructor() {
+    this._reservations = [];
+  }
+
+  addReservation(customer) {
+    this.zz_addReservation(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority === false);
+    this._reservations.push(customer);
+  }
+}
