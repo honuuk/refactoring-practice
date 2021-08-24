fd9a5f79411ebe55091577bef740ceac1306a3dd
jewook <jewook3617@gmail.com>
2021-08-24 18:09:44 +0900

    6-8 min, max 매개변수를 클래스로 묶기

---
+++ chapter6/#8-introduce-parameter-object/refactoring.js
@@ -0,0 +1,35 @@
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min, max };
+  }
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+  contains(arg) {
+    return arg >= this.min && arg <= this.max;
+  }
+}
+
+function readingsOutsideRange(station, range) {
+  return station.readings.filter((r) => range.contains(r.temp));
+}
+
+const station = {
+  name: "ZB1",
+  readings: [
+    { temp: 47, time: "2016-11-10 09:10" },
+    { temp: 53, time: "2016-11-10 09:20" },
+    { temp: 58, time: "2016-11-10 09:30" },
+    { temp: 53, time: "2016-11-10 09:40" },
+    { temp: 51, time: "2016-11-10 09:50" },
+  ],
+};
+
+const range = new NumberRange(
+  operationPlan.temperatureFloor.operationPlan.temperatureCeiling
+);
+
+const alerts = readingsOutsideRange(station, range);
