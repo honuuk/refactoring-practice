1ffa2e5a7c5a6a61208e9c71f226f6756f0b5d76
jewok3617 <jewook3617@gmail.com>
2021-09-15 22:24:37 +0900

    7-3 priority string을 클래스로 추출

---
+++ chapter7/#3-replace-primitive-with-object/refactoring.js
@@ -0,0 +1,18 @@
+class Order {
+	constructor(data) {
+			this.priority = data.priority;
+	}
+	get priorityString() { return this._priority.toString(); }
+	set priority(aString) { this._priority = new Priority(aString); }
+}
+
+class Priority {
+	constructor(value) {
+		this._value = value;
+	}
+	toString() { return this._value; }
+}
+
+const highPriorityCount = orders
+	.filter(o => "high" === o.priorityString || "rush" === o.priorityString)
+	.length;
