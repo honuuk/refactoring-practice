c118f19bcd1e31374f0864d08b31235ddb82577f
jewook <jewook3617@gmail.com>
2021-10-11 16:02:59 +0900

    8-7 두 가지 일을 하는 반복문을 두 개로 분리하기

---
+++ chapter8/#7-split-loop/refactoring.js
@@ -0,0 +1,11 @@
+let totalSalary = 0;
+for (const p of people) {
+  totalSalary += p.salary;
+}
+
+let youngest = people[0] ? people[0].age : Infinity;
+for (const p of people) {
+  if (p.age < youngest) youngest = p.age;
+}
+
+return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
